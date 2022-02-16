a5abdb40c4b2f1618c7ef7ea5b6cf2edc39d413a
Arthur Laurent <laurent.arthur75@gmail.com>
2022-02-16 22:37:06 +0100

    Update startup script

--- startup.py
+++ startup.py
@@ -1,72 +1,11 @@
-"""
-from app.core.settings import settings
-from app.backend.binance import binance
-from app.binance.account import *
-from pprint import pprint as pp
+import numpy as np
 import pandas as pd
-from time import sleep
-from app.helpers import *
-from datetime import datetime
-from app.analysis import *
+from datetime import datetime, timedelta
 
-t1 = datetime.now()
-#pp(dict(settings))
+from binance_analyst.core.settings import get_settings
+from binance_analyst.adapters import get_adapters
+from binance_analyst.repositories import get_repositories
 
-
-#pp(binance)
-#pp(binance.ping())
-#pp(binance.get_prices())
-#pp(binance.weight())
-#pp(binance.get_account_info())
-
-pp(Account.load().__dict__)
-btc = Coin("BTC")
-eth = Coin("ETH")
-print()
-pp([btc, eth])
-
-print()
-
-pp(btc.to(eth))
-#pp(btc.to(eth).get_klines())
-#pp(binance.get_prices())
-
-
-base_pairs = PairsManager.load()
-#pairs = PairsManager.filter(pairs, ["BTC", "ETH"])
-pairs = PairsManager.filter(
-    base_pairs,
-    ["BTC", "BNB", "LTC", "BAT", "ETH"],
-    exclusive=True)
-pairs = PairsManager.filter(
-    base_pairs,
-    ["BNB"],
-)
-dfs = PairsManager.load_dataframes(pairs)
-i = 6
-cdf = filter_dataframes(dfs)
-cdf = trim_dataframes(cdf)
-cdf = drop_missing_data_columns(cdf, perc=91)
-close = filter_dataframes(dfs)
-
-print(close)
-print()
-
-
-base_pairs = PairsManager.load()
-pairs = PairsManager.filter(base_pairs, ["BNB", "BTC"], exclusive=True)
-dfs = PairsManager.load_dataframes(pairs)
-df = filter_dataframes(dfs)
-
-print(RateOfReturn(df).simple())
-print(MovingAverage(df, periods=4).mean())
-
-
-
-
-
-t2 = datetime.now()
-tdiff = t2 - t1
-print(f"Took : {tdiff.seconds}.{tdiff.microseconds} seconds")
-print()
-"""
+s = get_settings()
+a = get_adapters()
+r = get_repositories()
