4a4c83bfde0997de3f4cca7ffce0d83cb92b0ee3
Arthur Laurent <laurent.arthur75@gmail.com>
2022-02-17 01:59:45 +0100

    Isort runned on startup script

--- startup.py
+++ startup.py
@@ -1,11 +1,14 @@
-import numpy as np
-import pandas as pd
 from datetime import datetime, timedelta
 
-from binance_analyst.core.settings import get_settings
+import numpy as np
+import pandas as pd
+
 from binance_analyst.adapters import get_adapters
+from binance_analyst.core.settings import get_settings
 from binance_analyst.repositories import get_repositories
 
 s = get_settings()
 a = get_adapters()
 r = get_repositories()
+
+pairs = r.pair.load()
