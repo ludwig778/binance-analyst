18f8fba5f12cd7fbd73d794bc97374ba90fc249a
Arthur Laurent <laurent.arthur75@gmail.com>
2022-02-17 22:37:22 +0100

    Create adapters directories if missing on startup script

--- startup.py
+++ startup.py
@@ -11,4 +11,9 @@ s = get_settings()
 a = get_adapters()
 r = get_repositories()
 
+if not a.metadata.dir_path.exists() or not a.dataframe.dir_path.exists():
+    print("Create default adapters directories")
+    a.metadata.create_dir()
+    a.dataframe.create_dir()
+
 pairs = r.pair.load()
